7ce0133c18cc545dbaf412f652411c490fa9b217
Gifty.p <83220370+gifty-cp@users.noreply.github.com>
2025-12-31 02:11:23 +0000

    Add message learning Python and GitHub

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# print("Hello, World!")
+# print Hello, World!
 print("Welcome to the Python program!")
 
 # print goodbye
 print("GoodBye")
+
+# print learning Python and GitHub
+print("I am learning Python and GitHub!")
